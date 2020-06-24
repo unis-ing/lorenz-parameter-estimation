5ca7a23d92a76c72f13e1f9626b9d25ec1a38d1e
unis-ing <neunice98@gmail.com>
2020-06-24 17:15:44 -0400

    broke up LPE.simulate into multiple methods, track pr within rule functions now

--- lorenz_pr_estimation.py
+++ lorenz_pr_estimation.py
@@ -7,111 +7,151 @@ import time
 
 de.logging_setup.rootlogger.setLevel('ERROR')
 
-NS = 2
 
 class LPE:
-	def __init__(self, PR=10, RA=28, B=8/3):
+	def __init__(self, PR=10, RA=28, B=8/3, NS=2):
 		"""
-			PR : true Prandtl
-			RA : true Rayleigh
-			B  : true physical parameter
-			pr : initial Prandtl guess
-			mu : nudging parameter
-			domain, problem, solver : dedalus objects
-			theta, rho, d : (for rule0_c1)  thresholds for update rule
+			PR : Prandtl number
+			RA : Rayleigh number
+			B  : physical parameter
+			NS : number of spectral basis modes
 		"""
 
 		self.PR = PR
 		self.RA = RA
 		self.B = B
+		self.NS = NS
 
 		basis = de.Chebyshev('s', NS, interval=(0,1), dealias=3/2)
 		domain = de.Domain([basis], grid_dtype=np.float64)
-
-		self.basis = basis
 
 		problem = de.IVP(domain, variables=['x', 'y', 'z', 'u', 'v', 'w', 'xt', 'yt', 'zt', 'ut', 'vt', 'wt'])
 		problem.parameters['PR'] = PR
 		problem.parameters['ra'] = RA
 		problem.parameters['b'] = B
 
+		self.basis = basis
 		self.domain = domain
 		self.problem = problem
 
-	def simulate(self, pr=2, mu=30, dt=1/2048, stop_iteration=1e3, pr_rule='no_update', nudge='x',
-				datafile='initial_data/lorenz_data.h5', filename='analysis', **kwargs):
+	def setup_rule(self, rule, **kwargs):
 		"""
-			pr : initial Prandtl number for assimilating system
-			mu : nudging parameter
-			dt : timestep
-			stop_iteration : length of simulation
-			pr_rule : (str) name of function that returns next pr
-			nudge : (str) coordinate(s) to nudge. Must be some combination of 'x', 'y', 'z'. Ex) 'x', 'xz', etc.
-			datafile : (str) h5 file. The last triple (x,y,z) in the time series will be taken as IC for lorenz equations.
-			filename : (str) name of analysis file
-			kwargs : extra arguments for pr_rule
+		store rule args as class attributes. return the function and arguments associated to the given rule.
+			rule  : (str) name of existing rule function
+			theta : position error threshold
+			rho   : velocity error threshold
+			d     : exponential decay factor for thresholds
+			Pc    : P* in writeup
+			P     : P in writeup
 		"""
-		self.pr = pr
-		self.mu = mu
-
-		domain = self.domain
-		problem = self.problem
-		problem.parameters['mu'] = mu
-
-		# get data from datafile
-		file = h5.File(datafile, 'r')
-		tasks = file['tasks']
-		x0 = np.array(tasks['x'])[-1][-1]
-		y0 = np.array(tasks['y'])[-1][-1]
-		z0 = np.array(tasks['z'])[-1][-1]
-		file.close()
-
-		#############################
-		# set pr_rule
-		#############################
-		if pr_rule == 'rule0_c1':
-			F = rule0_c1
+		# store constant rule args as class attributes
+		if rule == 'rule0_c1' or rule == 'rule1_c1':
 			self.theta = kwargs['theta']
 			self.rho = kwargs['rho']
 			self.d = kwargs['d']
-			thetalist = []
-			rholist = []
-		elif pr_rule == 'rule1_c1':
-			F = rule1_c1
-			self.theta = kwargs['theta']
-			self.rho = kwargs['rho']
-			self.d = kwargs['d']
-			thetalist = []
-			rholist = []
-			self.last = 0
-			self.P = kwargs['P']
-		elif pr_rule == 'no_update':
-			pass
+
+			# initialize empty lists for storage
+			self.thetalist = []
+			self.rholist = []
+
+			if rule == 'rule1_c1':
+				self.P = 0
+				self.Pc = kwargs['Pc']
+
+		# return rule args
+		pr0 = self.pr0
+		if rule == 'rule0_c1':
+			ruleargs = [self, {0 : pr0}] # dict to track updated pr
+			self.ruleargs = ruleargs
+			return rule0_c1, ruleargs
+
+		elif rule == 'rule1_c1':
+			ruleargs = [self, {0 : pr0}] # dict to track updated pr
+			self.ruleargs = ruleargs
+			return rule1_c1, ruleargs
+
+		elif rule == 'no_update':
+			return 1, 1
+
 		else:
-			print('Update rule has not been set. Exiting.')
-			return
+			return 0, 0
 
-		# add pr to problem parameters
-		if pr_rule == 'no_update':
-			problem.parameters['pr'] = pr
-		else:
-			problem.parameters['pr'] = de.operators.GeneralFunction(domain, layout='g', func=F, args=[])
-
-		# original lorenz equations
-		problem.add_equation('dt(x) = -PR*x + PR*y')
-		problem.add_equation('dt(y) + y = -PR*x - x*z')
-		problem.add_equation('dt(z) = -b*z + x*y - b*(ra+PR)')
-
-		# nudged lorenz equations
-		eqns = ['dt(u) = -pr*u + pr*v', 'dt(v) + v = -pr*u - u*w', 'dt(w) = -b*w + u*v - b*(ra+pr)']
+	def nudged_eqns(self, nudge):
+		"""
+		return nudged lorenz equations.
+		"""
+		eqns = ['dt(u) = -pr*u + pr*v', 
+				'dt(v) + v = -pr*u - u*w', 
+				'dt(w) = -b*w + u*v - b*(ra+pr)']
 		if 'x' in nudge:
 			eqns[0] += '- mu*(u-x)'
 		if 'y' in nudge:
 			eqns[1] += '- mu*(v-y)'
 		if 'z' in nudge:
 			eqns[2] += '- mu*(w-z)'
-		for eqn in eqns:
-			problem.add_equation(eqn)
+		return eqns
+
+	def update_ruleargs(self, rule, ruleargs):
+		"""
+		updates rulesargs at each iteration. also stores updated rule args.
+		"""
+		if rule == 'rule0_c1' or rule == 'rule1_c1':
+			self.thetalist.append(self.theta)
+			self.rholist.append(self.rho)
+
+			if rule == 'rule1_c1':
+				it = self.solver.iteration
+				prs = ruleargs[-1]
+				prev_pr = prs[it-1]
+				curr_pr = prs[it]
+				if curr_pr != prev_pr:
+					self.P = 0
+				else:
+					self.P += 1
+
+
+	def simulate(self, pr0=20, mu=30, dt=1/2048, stop_it=1000, rule='no_update', nudge='x', ts=de.timesteppers.RK443,
+				 ic='initial_data/lorenz_data.h5', outfile='analysis', **kwargs):
+		"""
+			pr0   : initial value for tilde Prandtl
+			mu    : nudging parameter
+			dt    : time-step
+			stop_it: number of iterations
+			rule  : update rule for tilde Prandtl
+			ts    : Dedalus time stepping scheme
+			nudge : (str) array or single string representing nudged coordinate(s)
+			ic    : (str) file path to h5 file containing initial conditions
+			out   : (str) file name for analysis to be saved under
+			kwargs: arguments for rule
+		"""
+		self.pr0 = pr0
+		self.mu = mu
+		self.rule = rule
+
+		domain = self.domain
+		problem = self.problem
+
+		# get pr update function
+		F, ruleargs = self.setup_rule(rule, **kwargs)
+		if not F: # invalid rule
+			print('No rule exists corresponding to the name ', rule, '. Exiting.')
+			return
+
+		# add pr to problem namespace
+		if F == 1: # no update
+			problem.parameters['pr'] = pr0
+		else:
+			problem.parameters['pr'] = de.operators.GeneralFunction(domain, layout='g', func=F, args=[])
+		problem.parameters['mu'] = mu
+
+		# lorenz equations
+		problem.add_equation('dt(x) = -PR*x + PR*y')
+		problem.add_equation('dt(y) + y = -PR*x - x*z')
+		problem.add_equation('dt(z) = -b*z + x*y - b*(ra+PR)')
+
+		# nudged lorenz equations
+		eqns = self.nudged_eqns(nudge)
+		[problem.add_equation(eqn) for eqn in eqns]
 
 		# d/dt equations
 		problem.add_equation('dt(x) - xt = 0')
@@ -121,81 +161,64 @@ class LPE:
 		problem.add_equation('dt(v) - vt = 0')
 		problem.add_equation('dt(w) - wt = 0')
 
-		solver = self.problem.build_solver(de.timesteppers.RK443)
+		if F != 1: # for some reason args must be set AFTER equations are added
+			problem.parameters['pr'].args = ruleargs
+			problem.parameters['pr'].original_args = ruleargs
+
+		# build solver
+		solver = problem.build_solver(ts)
 		self.solver = solver
 
-		x, y, z = solver.state['x'], solver.state['y'], solver.state['z']
-		u, v, w = solver.state['u'], solver.state['v'], solver.state['w']
-		xt, ut = solver.state['xt'], solver.state['ut']
-
 		# initialize lorenz system
+		file = h5.File(ic, 'r')
+		tasks = file['tasks']
+		x0 = np.array(tasks['x'])[-1, 0]
+		y0 = np.array(tasks['y'])[-1, 0]
+		z0 = np.array(tasks['z'])[-1, 0]
+		file.close()
+
+		x, y, z = solver.state['x'], solver.state['y'], solver.state['z']
+		NS = self.NS
 		x['g'] = np.full(NS, fill_value=x0)
 		y['g'] = np.full(NS, fill_value=y0)
 		z['g'] = np.full(NS, fill_value=z0)
-		u['g'] = np.zeros(NS)
-		v['g'] = np.zeros(NS)
-		w['g'] = np.zeros(NS)
-
-		#############################
-		# set arguments for pr_rule
-		#############################
-		if pr_rule == 'rule0_c1' or pr_rule == 'rule1_c1':
-			prlist = []
-			args = [pr, solver, self]
-			if pr_rule == 'rule1_c1':
-				args.append({})
-		if pr_rule == 'rule1_c1':
-			prev_pr = pr
-		if pr_rule != 'no_update':
-			problem.parameters['pr'].args = args
-			problem.parameters['pr'].original_args = args
-			self.rule_args = args
 
 		# set up analysis
-		analysis = solver.evaluator.add_file_handler(filename=filename, iter=1)
+		analysis = solver.evaluator.add_file_handler(filename=outfile, iter=1)
 		analysis.add_system(solver.state, layout='g')
 
 		solver.stop_sim_time = np.inf
-		solver.stop_iteration = stop_iteration
+		solver.stop_iteration = stop_it
 		start = time.time()
 
 		while solver.ok:
 			solver.step(dt)
 
-			# store pr
-			if pr_rule != 'no_update':
-				current_pr = val(problem.parameters['pr'])
-				prlist.append(current_pr)
+			if F != 1:
+				self.update_ruleargs(rule, ruleargs)
 
-			#############################
-			# update arg_rule arguments
-			#############################
-			if pr_rule != 'no_update': # applies to all
-				args[0] = current_pr
-			if pr_rule == 'rule0_c1' or pr_rule == 'rule1_c1':
-				thetalist.append(self.theta)
-				rholist.append(self.rho)
-			if pr_rule == 'rule1_c1':
-				if current_pr != prev_pr:
-					self.last = 0
-					prev_pr = current_pr
-				else:
-					self.last += 1
+			it = solver.iteration
+			percent = (it/stop_it)*100
 
-			percent = (solver.iteration/stop_iteration)*100
+			# print progress msg
 			if percent % 10 == 0:
-				prerr = abs(self.PR-current_pr)
-				print("{0:.0f}% complete. {1:.2f} sec elapsed. Abs pr error: {2:.8e}.".format(percent, time.time()-start, prerr))
+				progress_msg = '{0:.0f}% complete. {1:.2f} sec elapsed.'.format(percent, time.time()-start)
+				if F != 1:
+					prs = ruleargs[-1]
+					curr_pr = prs[it]
+					pr_err = abs(self.PR - curr_pr)
+					progress_msg += ' Abs pr error: {:.8e}.'.format(pr_err)
+				print(progress_msg)
 
 		print('Total runtime: {:.4f} sec.'.format(time.time()-start))
 
 		# write pr to .h5
-		path = '/'.join([filename, filename + '_s1', filename + '_s1_p0.h5'])
-		f = h5.File(path, 'a')
-		f.create_dataset('tasks/pr', data=prlist)
-		if pr_rule == 'rule0_c1' or pr_rule == 'rule1_c1':
-			f.create_dataset('tasks/theta', data=thetalist)
-			f.create_dataset('tasks/rho', data=rholist)
-		f.close()
+		if F != 1:
+			path = '/'.join([outfile, outfile + '_s1', outfile + '_s1_p0.h5'])
+			f = h5.File(path, 'a')
 
-		# adding comment
+			prs = list(prs.values())[1:]
+			f.create_dataset('tasks/pr', data=prs)
+			f.create_dataset('tasks/theta', data=self.thetalist)
+			f.create_dataset('tasks/rho', data=self.rholist)
+			f.close()
--- rules.py
+++ rules.py
@@ -6,28 +6,35 @@ def rule0(pr, mu, x, y, u, v):
 	return (pr*u - pr*v + mu*(u-x)) / (x-y)
 
 def rule0_c1(*args):
-	pr = args[0]
-	solver = args[1]
-	lpe = args[2]
+	lpe = args[0]
+	prs = args[1]
+	solver = lpe.solver
 
-	x = get(solver, 'x')
-	xt = get(solver, 'xt')
-	u = get(solver, 'u')
-	ut = get(solver, 'ut')
+ 	# add one bc this function is called to calculate the next pr
+	it = solver.iteration + 1
+	if it in prs:
+		return prs[it]
 
-	uerr = abs(x-u)
-	uterr = abs(xt-ut)
+	x, u, xt, ut = get(solver, 'x'), get(solver, 'u'), get(solver, 'xt'), get(solver, 'ut')
+	uerr, uterr = abs(x-u), abs(xt-ut)
+	pr = prs[it-1]
+	newpr = pr # initialize next pr as current pr
 
-	if (uerr<=lpe.theta) and (uterr<=lpe.rho) and (uerr>0) and (uterr>0):
+	c1 = uerr <= lpe.theta
+	c2 = uterr <= lpe.rho
+	c3 = (uerr > 0) and (uterr > 0)
+
+	if c1 and c2 and c3:
 		# decrease threshold
 		lpe.theta *= lpe.d
 		lpe.rho *= lpe.d
 		mu = lpe.mu
 		y = get(solver, 'y')
 		v = get(solver, 'v')
-		return rule0(pr, mu, x, y, u, v)
-	else:
-		return pr
+		newpr = rule0(pr, mu, x, y, u, v)
+
+	prs[it] = newpr
+	return newpr
 
 def rule1(pr, mu, x, u, v):
 	"""
@@ -36,35 +43,37 @@ def rule1(pr, mu, x, u, v):
 	return (pr*u - pr*v + mu*(u-x)) / (x-v)
 
 def rule1_c1(*args):
-	pr = args[0]
-	solver = args[1]
-	lpe = args[2]
-	prdict = args[3]
+	lpe = args[0]
+	prs = args[1]
+	solver = lpe.solver
 
-	x = get(solver, 'x')
-	xt = get(solver, 'xt')
-	u = get(solver, 'u')
-	ut = get(solver, 'ut')
+ 	# add one bc this function is called to calculate the next pr
+	it = solver.iteration + 1
+	if it in prs:
+		return prs[it]
 
-	uerr = abs(x-u)
-	uterr = abs(xt-ut)
+	x, u, xt, ut = get(solver, 'x'), get(solver, 'u'), get(solver, 'xt'), get(solver, 'ut')
+	uerr, uterr = abs(x-u), abs(xt-ut)
+	pr = prs[it-1]
+	newpr = pr # initialize next pr as current pr
 
-	print(solver.iteration, ' ', uerr, ' ', uterr)
-	it = solver.iteration
-	if it in prdict:
-		return prdict[it]
+	c1 = lpe.P >= lpe.Pc
+	c2 = uerr <= lpe.theta
+	c3 = uterr <= lpe.rho
+	c4 = True
+	# c4 = it >= 5000
+	c5 = (uerr > 0) and (uterr > 0)
 
-	newpr = pr
-	# if (solver.iteration>=5000) and (lpe.last>=lpe.P) and (uerr<=lpe.theta) and (uterr<=lpe.rho) and (uerr>0) and (uterr>0):
-	if (lpe.last>=lpe.P) and (uerr<=lpe.theta) and (uterr<=lpe.rho) and (uerr>0) and (uterr>0):
+	if c1 and c2 and c3 and c4 and c5:
 		# decrease thresholds
 		lpe.theta *= lpe.d
 		lpe.rho *= lpe.d
 		mu = lpe.mu
 		v = get(solver, 'v')
 		newpr = rule1(pr, mu, x, u, v)
+		# print(it, uerr, uterr)
 
-	prdict[solver.iteration] = newpr
+	prs[it] = newpr
 	return newpr
 
 def get(solver, s):
